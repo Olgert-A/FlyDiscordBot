d1bff169da26fcb8b2ab5c19aba7722f7f9c30f6
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-09-20 20:53:44 +0500

    Update register.py

--- commands/register.py
+++ commands/register.py
@@ -2,4 +2,4 @@ from commands.fun import cmd_hofik, cmd_pidor, cmd_droch
 from levels.commands import *
 
 to_use = [cmd_hofik, cmd_pidor, cmd_droch,
-          cmd_levels_reg, cmd_levels_stop, cmd_levels_points, cmd_levels_table, cmd_levels_kick, cut]
+          cmd_levels_reg, cmd_levels_stop, cmd_levels_points, cmd_levels_table, cmd_levels_kick]
