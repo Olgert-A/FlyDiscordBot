273f91e59eaed52168e3726f0a9fd048f77cd94f
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-09-23 23:40:16 +0500

    fix

--- levels/events.py
+++ levels/events.py
@@ -110,12 +110,9 @@ class LevelEvents:
 
         team_size = random.randint(2, int(members_amount / 2))
         selected = random.sample(members, team_size * 2)
-        logging.info(selected)
 
-        team1 = selected[:len(selected)]
-        team2 = selected[:-len(selected)]
-        logging.info(team1)
-        logging.info(team2)
+        team1 = selected[:len(selected)/2]
+        team2 = selected[:-len(selected)/2]
 
         report = 'Командная ебка!\n\nКоманда №1:\n'
         for m in team1:
@@ -136,11 +133,11 @@ class LevelEvents:
         for m_id, m_pts in team1_pts.items():
             pts = int(m_pts / kick_result)
             get_levels_db().points_add(channel_id, m_id, pts)
-            report += f'<@{m_id}>\n получает {LevelPoints.convert(pts):.2f} см.\n'
+            report += f'<@{m_id}> получает {LevelPoints.convert(pts):.2f} см.\n'
 
         for m_id, pts in team2_pts.items():
             pts = -int(pts / kick_result)
             get_levels_db().points_add(channel_id, m_id, pts)
-            report += f'<@{m_id}>\n получает {LevelPoints.convert(pts):.2f} см.\n'
+            report += f'<@{m_id}> получает {LevelPoints.convert(pts):.2f} см.\n'
 
         return report
