ca22f4fa35039b3ae8959715f7ddffeed18148f5
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-09-23 23:25:34 +0500

    kick refactor

--- levels/utils/kick.py
+++ levels/utils/kick.py
@@ -18,19 +18,21 @@ class LevelKick:
         get_kicks_db().add(channel_id, author_id, 1)
 
     @staticmethod
-    def calc(channel_id, author_id, target_id) -> int:
-        author_pts = LevelPoints.get(channel_id, author_id)
-        target_pts = LevelPoints.get(channel_id, target_id)
-        logging.info(f'author_pts {author_pts}, target_pts {target_pts}')
-        #if author_pts is None or target_pts is None:
-        #    raise ValueError
+    def calc_by_pts(author_pts, target_pts):
         pts = random.randint(0, abs(author_pts - target_pts))
         chance = random.randint(-author_pts, target_pts) / max([abs(author_pts), abs(target_pts), 1])
         return int(pts * chance)
 
     @staticmethod
+    def calc_by_id(channel_id, author_id, target_id) -> int:
+        author_pts = LevelPoints.get(channel_id, author_id)
+        target_pts = LevelPoints.get(channel_id, target_id)
+        logging.info(f'author_pts {author_pts}, target_pts {target_pts}')
+        return LevelKick.calc_by_pts(author_pts, target_pts)
+
+    @staticmethod
     def execute(channel_id, author_id, target_id):
-        pts_up = LevelKick.calc(channel_id, author_id, target_id)
+        pts_up = LevelKick.calc_by_id(channel_id, author_id, target_id)
         get_levels_db().points_add(channel_id, author_id, pts_up)
         get_levels_db().points_add(channel_id, target_id, -pts_up)
         return pts_up
