3fe96ea6119c19ebe532cf03ac77c41ae429793c
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-09-30 20:11:07 +0500

    fix

--- main.py
+++ main.py
@@ -14,33 +14,19 @@ logging.basicConfig(level=logging.INFO)
 def main():
     client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-    @client.tree.command(name='hof1k', description='Find hof1k % in yourself')
-    async def hof1k(ctx: discord.Interaction):
-        await ctx.response.send_message(f"Сегодня ты хофик на {random.randint(0, 100)}%!")
-
     @client.event
     async def on_ready():
         print(f"{client.user.name} connected.")
+
+        await client.tree.sync()
+
         kicks_daily_clear.start()
         channels = [client.get_channel(channel_id) for channel_id in get_levels_db().get_channels()]
         level_daily_event.start(channels)
 
-    @client.tree.command(name='hello')
-    async def hello(interaction: discord.Interaction):
-        await interaction.response.send_message('Slash hello')
-
-    @client.tree.command(name='pidor')
-    async def pidor(interaction: discord.Interaction):
-        await interaction.response.send_message('Ty pidor')
-
-    @commands.command(name='sync')
-    async def cmd_sync(ctx):
-        if not (ctx.author.id == 776537982924619786):
-            await ctx.message.reply(f"Sasi <:pepe_loh:1022083481725063238>")
-            return
-
-        synced = await client.tree.sync()
-        await ctx.message.reply(f'Commands synced: {len(synced)}')
+    @client.tree.command(name='hof1k', description='Find hof1k % in yourself')
+    async def hof1k(ctx: discord.Interaction):
+        await ctx.response.send_message(f"Сегодня ты хофик на {random.randint(0, 100)}%!")
 
     @client.event
     async def on_message(message):
@@ -54,7 +40,6 @@ def main():
     for cmd in to_use:
         client.add_command(cmd)
 
-    client.add_command(cmd_sync)
     client.run(os.getenv("DISCORD_TOKEN"))
 
 
