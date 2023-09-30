9ca444e4d4611229433206e8d2b375f03e65f80c
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-09-30 22:33:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
         await ctx.response.send_message(f"Сегодня ты хофик на {random.randint(0, 100)}%!")
 
     @client.tree.command(name='любовь')
-    async def hof1k(ctx: discord.Interaction, source: commands.Greedy[Union[str, discord.Member]], destination: str):
+    async def hof1k(ctx: discord.Interaction, source: commands.Greedy[str, discord.Member], destination: str):
         def format_param(param):
             return f'<@{param.id}>' if isinstance(param, discord.Member) else param
 
