ac4f20113272dc6cf3fcdc17926a5a42facfb112
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-09-30 23:14:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
         await ctx.response.send_message(f"Сегодня ты хофик на {random.randint(0, 100)}%!")
 
     @client.tree.command(name='любовь', description='Проверь любовь этих двоих')
-    @app_commands.rename(who='кто', target='цель')
+    @app_commands.rename(who='чья', target='к-кому')
     @app_commands.describe(who='Чью любовь проверяешь', target='Любовь к кому/чему проверяешь')
     async def hof1k(ctx: discord.Interaction, who: str, target: str):
         def format_param(param):
