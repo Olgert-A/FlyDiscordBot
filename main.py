c5c0c711bfb82af391f6acdd8eacf0cd7cf3706f
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-10-04 02:53:18 +0500

    fix

--- main.py
+++ main.py
@@ -3,24 +3,20 @@ import discord
 import logging
 import asyncio
 from discord.ext import commands
-from commands.register import register_commands
-from db.current import get_levels_db
-from levels.tasks import kicks_daily_clear, level_daily_event
 
 logging.basicConfig(level=logging.INFO)
 
 
-def main():
+async def main():
     client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-    for f in os.listdir("./cogs"):
-        if f.endswith(".py"):
-            client.load_extension("cogs." + f[:-3])
+    cogs = ['cogs.fun', 'cogs.listeners', 'cogs.levels']
+    for cog in cogs:
+        await client.load_extension(cog)
 
-    register_commands(client)
-    #    await register_cogs(client)
-    client.run(os.getenv("DISCORD_TOKEN"))
+    await client.start(os.getenv("DISCORD_TOKEN"))
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
+
