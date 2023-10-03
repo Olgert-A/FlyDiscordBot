2b198afec669ad382a70408e2ae845296b846b2d
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-10-04 02:41:58 +0500

    fix

--- main.py
+++ main.py
@@ -8,30 +8,19 @@ from db.current import get_levels_db
 from levels.tasks import kicks_daily_clear, level_daily_event
 
 logging.basicConfig(level=logging.INFO)
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-
-@client.event
-async def on_ready(self):
-    logging.info(f"{self.bot.user.name} connected.")
-    cogs = ['cogs.listeners', 'cogs.fun']
-    for cog in cogs:
-        await self.bot.load_extension(cog)
-
-    await self.bot.tree.sync()
-
-    kicks_daily_clear.start()
-    channels = [self.bot.get_channel(channel_id) for channel_id in get_levels_db().get_channels()]
-    level_daily_event.start(channels)
 
 
 def main():
+    client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+    for f in os.listdir("./cogs"):
+        if f.endswith(".py"):
+            client.load_extension("cogs." + f[:-3])
+
     register_commands(client)
-#    await register_cogs(client)
+    #    await register_cogs(client)
     client.run(os.getenv("DISCORD_TOKEN"))
 
 
 if __name__ == '__main__':
     main()
-
-
