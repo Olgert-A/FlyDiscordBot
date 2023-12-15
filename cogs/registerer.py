d40c47d9918b346a44f67aedd1d9d72c98f0e1b7
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-12-15 23:45:14 +0500

    Update registerer.py

--- cogs/registerer.py
+++ cogs/registerer.py
@@ -2,6 +2,6 @@ from discord.ext import commands
 
 
 async def register_cogs(bot: commands.Bot):
-    cogs = ['cogs.fun', 'cogs.listeners', 'cogs.levels']
+    cogs = ['cogs.fun', 'cogs.listeners', 'cogs.levels', 'cogs.rolls']
     for cog in cogs:
         await bot.load_extension(cog)
