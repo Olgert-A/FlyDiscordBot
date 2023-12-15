9f58896849e7adf8f87332e5a0f00058b9a02a56
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2023-12-16 00:28:53 +0500

    Update rolls.py

--- db/postgres/rolls.py
+++ db/postgres/rolls.py
@@ -9,12 +9,14 @@ logging.basicConfig(level=logging.INFO)
 class RollsDb(AbstractRollsDB):
     def __init__(self, database_url):
         super().__init__(database_url)
+        self._drop_tables()
         self._guilds_create()
         self._rolls_create()
 
     def _drop_tables(self):
+        logging.debug('dropped')
         with psycopg.connect(self.DATABASE_URL) as c:
-            c.execute("""DROP TABLE IF EXISTS guilds; DROP TABLE IF EXISTS rolls;""")
+            c.execute("""DROP TABLE IF EXISTS guilds, rolls;""")
 
     def _guilds_create(self):
         with psycopg.connect(self.DATABASE_URL) as c:
