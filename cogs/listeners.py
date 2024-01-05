564ecf4f85f3c86aefa2ffae01097d6f89934199
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2024-01-05 16:55:12 +0500

    change task start

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,6 +1,6 @@
 import logging
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 from db.current import get_levels_db, get_rolls_db
 from levels.utils.points import LevelPoints
 from levels.tasks import kicks_daily_clear, level_daily_event, roll_points_event
@@ -15,11 +15,26 @@ class ListenerCog(commands.Cog):
     async def bot_init(self):
         await self.bot.tree.sync()
 
-        kicks_daily_clear.start()
+        def tack_starter(task_coroutine, params=None):
+            task = task_coroutine.get_task()
+            if task and not task.done():
+                logging.info(f"task {task} already runned!")
+                return
+            if params:
+                task_coroutine.start(params)
+            else:
+                task_coroutine.start()
+            logging.info(f"task {task} is starting!")
+
+        tack_starter(kicks_daily_clear)
+        #kicks_daily_clear.start()
         channels = [self.bot.get_channel(channel_id) for channel_id in get_levels_db().get_channels()]
-        level_daily_event.start(channels)
+        #level_daily_event.start(channels)
+        tack_starter(level_daily_event, channels)
         guilds = [self.bot.get_guild(guild_id) for guild_id in get_rolls_db().get_guilds()]
-        roll_points_event.start(guilds)
+        tack_starter(roll_points_event, guilds)
+        #roll_points_event.start(guilds)
+
 
     @commands.Cog.listener()
     async def on_ready(self):
