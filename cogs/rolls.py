8df2402d813b9524976369b61537e2179b717220
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2026-09-03 20:24:11 +0500

    Update rolls.py

--- cogs/rolls.py
+++ cogs/rolls.py
@@ -41,7 +41,10 @@ class RollsCog(commands.Cog):
         self.factor_index = 6
         self.huesos_factor  = [-1, -1, 1, 1, 1, 1]
         self.huesos_index = 6
-    
+        self.group_roll_users = []
+        self.group_roll_factor = [1, 1, 1, 1, 1, 1, 1, 1, 1, -1]
+        self.group_roll_index = -1
+   
     def get_win_sign(self):
         self.factor_index += 1
         if self.factor_index >= len(self.random_factor):
@@ -56,7 +59,7 @@ class RollsCog(commands.Cog):
             self.huesos_index = 0
             random.shuffle(self.huesos_factor)
             
-        return self.huesos_factor[self.huesos_index]
+        return self.huesos_factor[self.huesos_index]      
 
     def duels_add(self, message_id, user_id, target_id, points, timestamp):
         logging.info(f'add contract: {message_id}--{user_id}--{target_id}--{points}--{timestamp}')
@@ -133,8 +136,48 @@ class RollsCog(commands.Cog):
         random.shuffle(win_texts)
         random.shuffle(lose_texts)      
         await ctx.followup.send(f"{name(ctx.user)} признался в том, что он хуесос и {win_texts[0] if win_sign == 1 else lose_texts[0]}! Теперь на счету сердечек: {user_pts + pts_to_add}!")
+
+    @app_commands.command(name='голландский_штурвал', description='Групповая рулетка всех сердечек')
+    async def group_roll(self, ctx: discord.Interaction):
+        await ctx.response.defer()
+
+        if len(self.group_roll_users) == 0:
+            random.shuffle(self.group_roll_factor)
+            self.group_roll_index = -1
+
+        user_exist = -1
+        for user in self.group_roll_users:
+            if ctx.user.id == user:
+                user_exist = 1
+                
+        if user_exist == 1:
+            result = "Ты уже участвуешь в групповой крутке! Список участников: "
+        else:
+            self.group_roll_users.append(ctx.user.id)
+            result = "Новый участник групповой крутки! Список участников: "
         
-    
+        self.group_roll_index += 1
+        win_sign = self.group_roll_factor[self.group_roll_index]
+        
+        for user in self.group_roll_users:
+            result += f"{name(user)}"
+            if user == self.group_roll_users[len(self.group_roll_users) - 1]:
+                result += "."
+            else:
+                result += ", "
+            
+            user_pts = get_rolls_db().points_get(ctx.guild.id, user)
+            pts_to_add = win_sign * user_pts
+            get_rolls_db().points_add(ctx.guild.id, user, pts_to_add)
+            
+        if win_sign == 1:
+            result += "Выигрывают! Сердечки всех участников удвоены!"
+        else:
+            result += "Проиграли! На счету каждого участника 0 сердечек. Групповая крутка закончена."
+            self.group_roll_users.clear()
+
+        await ctx.followup.send(result)
+            
     @app_commands.command(name='крутить',
                           description='Рулетка сердечек')
     @app_commands.rename(pts_arg='сердечки')
